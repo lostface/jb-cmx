0bdd484a60f18b61c618df9f5e83266a37fac1b0
Zoltan Feher <lostface@fastmail.fm>
2016-10-10 10:56:50 +0200

    Add should comp update to hero detail dialog

--- app/components/hero-detail-dialog.js
+++ app/components/hero-detail-dialog.js
@@ -3,91 +3,101 @@ import HeroStatsChart from './hero-stats-chart';
 import HeroLinks from './hero-links';
 import { Button, Dialog, DialogTitle, DialogActions, DialogContent } from 'react-mdl';
 
-export default function HeroDetailDialog(props) {
-  const { show, hero, onCloseTrigger } = props;
-  const statsAvailable = hero.comics.available > 0
-    || hero.stories.available > 0
-    || hero.events.available > 0
-    || hero.series.available > 0;
+export default React.createClass({
+  propTypes: {
+    show: React.PropTypes.bool.isRequired,
+    hero: React.PropTypes.shape({
+      id: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired,
+      thumbnail: React.PropTypes.string.isRequired,
+      urls: React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          type: React.PropTypes.string.isRequired,
+          url: React.PropTypes.string.isRequired,
+        })
+      ),
+      comics: React.PropTypes.shape({
+        available: React.PropTypes.number.isRequired
+      }),
+      stories: React.PropTypes.shape({
+        available: React.PropTypes.number.isRequired
+      }),
+      events: React.PropTypes.shape({
+        available: React.PropTypes.number.isRequired
+      }),
+      series: React.PropTypes.shape({
+        available: React.PropTypes.number.isRequired
+      }),
+    }),
+    onCloseTrigger: React.PropTypes.func,
+  },
 
-  return (
-    <div>
-      <Dialog open={show} onCancel={onCloseTrigger} onClick={handleClick} style={{width: '40%'}} >
-        <DialogTitle
-            style={{
-              marginTop: -11,
-              padding: '29px 24px 25px 74px',
-              background: `url(${hero.thumbnail}) left/64px no-repeat`
-            }}>
-          {hero.name}
-        </DialogTitle>
+  getDefaultProps() {
+    return {
+      show: false,
+      hero: {
+        id: -1,
+        name: '',
+        description: '',
+        thumbnail: '',
+        urls: [],
+        comics: { available: 0 },
+        stories: { available: 0 },
+        events: { available: 0 },
+        series: { available: 0 },
+      },
+    };
+  },
 
-        <DialogContent>
-          <p>{hero.description}</p>
+  render() {
+    const { show, hero, onCloseTrigger } = this.props;
+    const statsAvailable = hero.comics.available > 0
+      || hero.stories.available > 0
+      || hero.events.available > 0
+      || hero.series.available > 0;
 
-          <h4>Links</h4>
-          <HeroLinks hero={hero} />
+    return (
+      <div>
+        <Dialog open={show} onCancel={onCloseTrigger} onClick={this.handleClick} style={{width: '40%'}} >
+          <DialogTitle
+              style={{
+                marginTop: -11,
+                padding: '29px 24px 25px 74px',
+                background: `url(${hero.thumbnail}) left/64px no-repeat`
+              }}>
+            {hero.name}
+          </DialogTitle>
 
-          <h4>Stats</h4>
-          { statsAvailable ? <HeroStatsChart hero={hero} /> : null }
-        </DialogContent>
+          <DialogContent>
+            <p>{hero.description}</p>
 
-        <DialogActions>
-          <Button onClick={onCloseTrigger}>Gotcha</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+            <h4>Links</h4>
+            <HeroLinks hero={hero} />
 
-  function handleClick(event) {
+            <h4>Stats</h4>
+            { statsAvailable ? <HeroStatsChart hero={hero} /> : null }
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={onCloseTrigger}>Gotcha</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  },
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  },
+
+  handleClick(event) {
     const targetTagName = event.target.tagName.toLowerCase();
+    const { onCloseTrigger } = this.props;
+
     // when clicked outside
     if (targetTagName == 'dialog') {
       onCloseTrigger();
     }
-  }
-}
-
-HeroDetailDialog.propTypes = {
-  show: React.PropTypes.bool.isRequired,
-  hero: React.PropTypes.shape({
-    id: React.PropTypes.number.isRequired,
-    name: React.PropTypes.string.isRequired,
-    description: React.PropTypes.string.isRequired,
-    thumbnail: React.PropTypes.string.isRequired,
-    urls: React.PropTypes.arrayOf(
-      React.PropTypes.shape({
-        type: React.PropTypes.string.isRequired,
-        url: React.PropTypes.string.isRequired,
-      })
-    ),
-    comics: React.PropTypes.shape({
-      available: React.PropTypes.number.isRequired
-    }),
-    stories: React.PropTypes.shape({
-      available: React.PropTypes.number.isRequired
-    }),
-    events: React.PropTypes.shape({
-      available: React.PropTypes.number.isRequired
-    }),
-    series: React.PropTypes.shape({
-      available: React.PropTypes.number.isRequired
-    }),
-  }),
-  onCloseTrigger: React.PropTypes.func,
-};
-
-HeroDetailDialog.defaultProps = {
-  show: false,
-  hero: {
-    id: -1,
-    name: '',
-    description: '',
-    thumbnail: '',
-    urls: [],
-    comics: { available: 0 },
-    stories: { available: 0 },
-    events: { available: 0 },
-    series: { available: 0 },
   },
-};
+});
