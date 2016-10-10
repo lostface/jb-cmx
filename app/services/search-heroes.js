3fad6a0a012dae313634dad825b9e7efcbe42cf0
Zoltan Feher <lostface@fastmail.fm>
2016-10-10 17:02:02 +0200

    Remove temporary solution for fetching heroes

--- app/services/search-heroes.js
+++ app/services/search-heroes.js
@@ -1,18 +1,12 @@
 import * as R from 'ramda';
-// TODO temporary solution should be removed !!
-import md5 from 'md5';
-import { MARVEL_API_PRIVATE_KEY } from './_temp';
 
 const MARVEL_API_PUBLIC_KEY = 'b06b10c45f5682a08c60afa345fcf261';
 const MARVEL_API_BASE_URL = 'https://gateway.marvel.com';
 const MARVEL_API_CHARACTERS_URL = `${MARVEL_API_BASE_URL}/v1/public/characters`;
 
 export default function searchHeroes(searchText) {
-  // TODO temporary solution, should be removed !!
-  const ts = String(Date.now());
-  const hash = md5(ts + MARVEL_API_PRIVATE_KEY + MARVEL_API_PUBLIC_KEY);
   const url = encodeURI(
-    `${MARVEL_API_CHARACTERS_URL}?nameStartsWith=${searchText}&ts=${ts}&hash=${hash}&apikey=${MARVEL_API_PUBLIC_KEY}`
+    `${MARVEL_API_CHARACTERS_URL}?nameStartsWith=${searchText}&apikey=${MARVEL_API_PUBLIC_KEY}`
   );
 
   return fetch(url)
